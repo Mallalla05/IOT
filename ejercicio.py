d112409714ad7ea8440b25c312e6f179c9fe37f7
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-11-06 08:09:55 -0600

    ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -2,5 +2,5 @@ import streamlit as st
 import pandas as pd
 
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
-df=pd.read_csv("Police1.csv")
+df=pd.read_csv("Policev1.csv")
 df
