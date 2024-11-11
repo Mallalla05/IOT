8115af52484c92e29e8eb74d451661ca80bbaed4
Mallalla05 <150102288+Mallalla05@users.noreply.github.com>
2024-11-11 07:53:25 -0600

    ejercicio.py

--- ejercicio.py
+++ ejercicio.py
@@ -1,28 +1,28 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
-df=pd.read_csv("Policev1.csv")
 
-st.markdown('The data show below belong to incident reports in the city os San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
-mapa=pd.DataFrame()
-mapa['Date']=df ['Incident Date']
-mapa['Day']=df ['Incident Day of Week']
-mapa['Police District']=df ['Police District']
-mapa['Neighborhood']=df ['Analysis Neighborhood']
-mapa['Incident Category']=df ['Incident Category']
-mapa['Incident Subcategory']=df ['Incident subcategory']
-mapa['Resolution']=df ['Resolution']
-mapa['lat']=df ['Latitud']
-mapa['lon']=df ['Longitud']
-mapa=maoa.dropna()
+df = pd.read_csv("Policev1.csv")
 
-subset_data2=mapa
-police_district_input=st.sidebar.multiselect(
-  'Police District',
-  mapa.groupby('Police District').count().reset_index()['Police District'].tolist())
-if len(police_district_input)>0:
-  subset_data2=mapa[mapa['Police District'].isin(police_district_input)]
+st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
+mapa = pd.DataFrame()
+mapa['Date'] = df['Incident Date']
+mapa['Day'] = df['Incident Day of Week']
+mapa['Police District'] = df['Police District']
+mapa['Neighborhood'] = df['Analysis Neighborhood']
+mapa['Incident Category'] = df['Incident Category']
+mapa['Incident Subcategory'] = df['Incident Subcategory']
+mapa['Resolution'] = df['Resolution']
+mapa['lat'] = df['Latitude']
+mapa['lon'] = df['Longitude']
+mapa = mapa.dropna()
+
+subset_data2 = mapa
+police_district_input = st.sidebar.multiselect(
+'Police District',
+mapa.groupby('Police District').count().reset_index()['Police District'].tolist())
+if len(police_district_input) > 0:
+    subset_data2 = mapa[mapa['Police District'].isin(police_district_input)]
 
